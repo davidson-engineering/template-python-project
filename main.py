46843bac349c17927c526347069d0a49784b618c
Matthew Davidson <admin@davidson.engineering>
2024-09-07 16:17:01 -0700

    Added python-config-loader module to facilitate loading of user configurations

--- main.py
+++ main.py
@@ -8,9 +8,24 @@
 """a_short_project_description"""
 # ---------------------------------------------------------------------------
 
+import logging
+from logging.config import dictConfig
+
+# Import the load_configs function
+from config_loader import load_configs
+
+LOGGING_CONFIG_FILEPATH = "config/logging.yaml"
+APP_CONFIG_FILEPATH = "config/application.toml"
+
+# Load user configurations using the config_loader module
+configs = load_configs([APP_CONFIG_FILEPATH, LOGGING_CONFIG_FILEPATH])
+
+# Configure logging using the specified logging configuration
+dictConfig(configs["logging"])
+
 
 def main():
-    pass
+    logging.info(configs["application"])
 
 
 if __name__ == "__main__":
